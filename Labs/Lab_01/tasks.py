aa135f6fbb8cc34907897d9c021a8f9f4bb22690
mirossha <m.dyakonov@g.nsu.ru>
2026-09-08 16:54:55 +0700

    Complete Lab 01

--- Labs/Lab_01/tasks.py
+++ Labs/Lab_01/tasks.py
@@ -30,14 +30,15 @@ name = input("Enter your name: ")
 # Remember that input() returns a string.
 
 age = 0
-
+age = int(input("Enter your age: "))
 # TODO:
 # Print:
 # Hello, <name>!
 # Next year you will be <age + 1> years old.
+print(f"Hello, {name}!")
+print(f"Next year you will be {age + 1} years old.")
 
 
-print()
 
 
 # ============================================================
@@ -51,22 +52,22 @@ print("Task 2 — Rectangle")
 
 width = 0.0
 height = 0.0
-
+width = float(input("width: "))
+height = float(input("height: "))
 # TODO:
 # Calculate the area.
 
 area = 0.0
-
+area = width * height
 # TODO:
 # Calculate the perimeter.
 
 perimeter = 0.0
+perimeter = 2 * (width + height)
 
 # TODO:
-# Print the results.
-
-
-print()
+print(f"Area: {area}")
+print(f"Perimeter: {perimeter}")
 
 
 # ============================================================
@@ -82,18 +83,17 @@ print("Task 3 — Temperature Converter")
 # Read Celsius temperature.
 
 celsius = 0.0
-
+celsius = float(input("Enter temperature in celsius: "))
 # TODO:
 # Calculate Fahrenheit temperature.
 
 fahrenheit = 0.0
-
+fahrenheit = celsius * 9 / 5 + 32
 # TODO:
 # Print the result.
 
 
-print()
-
+print(f"Temperature in fahrenheit: {fahrenheit}")
 
 # ============================================================
 # Task 4 — Purchase Calculator
@@ -105,27 +105,29 @@ print("Task 4 — Purchase Calculator")
 # Ask for the number of items.
 
 quantity = 0
-
+quantity = int(input("Enter number of items: "))
 # TODO:
 # Ask for the price of one item.
 
 price = 0.0
+price = float(input("Enter price of an item: "))
 
 # TODO:
 # Calculate the total price.
 
 total_price = 0.0
-
+total_price = quantity * price
 # TODO:
 # Apply a 10% discount.
 
 discounted_price = 0.0
-
+discounted_price = total_price * 0.9
 # TODO:
 # Print both results.
 
 
-print()
+print(f"Price before discount: {total_price:.2f}")
+print(f"Price with the discount: {discounted_price:.2f}")
 
 
 # ============================================================
@@ -147,3 +149,11 @@ b = 5
 # a // b
 # a % b
 # a ** b
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)
+print(a % b)
+print(a ** b)
